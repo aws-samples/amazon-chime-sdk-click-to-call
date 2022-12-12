16fcf49c769f22fb17c5354bd21d896ee113dcd4
Court Schuett <schuettc@amazon.com>
2022-12-12 23:21:10 +0000

    fix: change region

--- src/amazon-chime-sdk-click-to-call.ts
+++ src/amazon-chime-sdk-click-to-call.ts
@@ -60,7 +60,7 @@ export class AmazonChimeSDKClickToCall extends Stack {
 
 const devEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+  region: 'us-east-1',
 };
 
 const app = new App();
